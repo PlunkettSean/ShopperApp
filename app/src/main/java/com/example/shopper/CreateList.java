ee8b7b32c60a65b9d801737f6d3171f0522d00bb
Sean Plunkett <seanplunkett474@gmail.com>
2021-02-02 10:48:09 -0500

    Tuesday, February 2, 2021 Made the CreateList page, and made app runnable.

--- app/src/main/java/com/example/shopper/CreateList.java
+++ app/src/main/java/com/example/shopper/CreateList.java
@@ -1,5 +1,6 @@
 package com.example.shopper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,9 +12,26 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class CreateList extends AppCompatActivity {
+    // declare intent
+    Intent intent;
 
+    // declare EditText;
+    EditText nameEditText;
+    EditText storeEditText;
+    EditText dateEditText;
+
+    // declare Calender
+    Calendar calendar;
+
+    /**
+     * This method initializes the Action Bar and View of the activity.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +39,18 @@ public class CreateList extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // initialize EditTexts
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        storeEditText = (EditText) findViewById(R.id.storeEditText);
+        dateEditText = (EditText) findViewById(R.id.dateEditText);
     }
 
+    /**
+     * This method further initializes the Action Bar of the activity.
+     * It gets the code (XML) in the menu resource file and incorporates it into the Action Bar.
+     * @param menu menu resource file for the activity.
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,19 +58,29 @@ public class CreateList extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * This method gets called when a menu item in the overflow menu is selected and it
+     * controls what happens when the menu item is selected.
+     * @param item selected item in the overflow menu.
+     * @return true if the menu item is selected, else false.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_home) {
-            return true;
+        // get the id of the menu item selected
+        switch(item.getItemId()) {
+            case R.id.action_home :
+                // initialize an Intent for the MainActivity and start it.
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_create_list :
+                // initialize an Intent for the CreateList Activity and start it.
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void createList(MenuItem item) {
